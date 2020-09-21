3fe51dcee54d029ce775254cc2b2e44a5c6e16bf
5at05h1 <dfcbvghl_3104@icloud.com>
2020-09-21 20:36:44 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,9 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :name, :surname, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+    validates :nickname
+    validates :email
+    validates :birthday
     validates :name_kana, :surname_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
   end
 end
