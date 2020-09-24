925fa84dcaaf18720c0c87741b37b9dc9f05bfa7
5at05h1 <dfcbvghl_3104@icloud.com>
2020-09-24 20:00:57 +0900

    カテゴリー選択実装１

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,12 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :shipping
   belongs_to :user
   has_one_attached :image
+
+  validates :name, :comment, :category, :status, :shipping, :delivery, :date, :price, :user, presence: true
+
+  validates :category_id, :status_id, :shipping_id, :delivery_id, :date_id, numericality: { other_than: 1 }
 end
