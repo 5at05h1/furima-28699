0ff69f5e34c1e19686f004eb7ebea35df773a385
5at05h1 <dfcbvghl_3104@icloud.com>
2020-09-27 21:09:30 +0900

    DB保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,20 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def new
+    @item = Item.new
   end
 
-  
+  def create
+    Item.create!(item_params)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :comment, :category_id, :status_id, :shipping_id, :delivery_id, :s_date_id, :price).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,10 +4,11 @@ class Item < ApplicationRecord
   belongs_to_active_hash :status
   belongs_to_active_hash :shipping
   belongs_to_active_hash :delivery
+  belongs_to_active_hash :s_date
   belongs_to :user
   has_one_attached :image
 
-  validates :name, :comment, :category_id, :status_id, :shipping_id, :delivery_id, :date_id, :price, :user, presence: true
+  validates :name, :comment, :category_id, :status_id, :shipping_id, :delivery_id, :s_date_id, :price, :user, presence: true
 
-  validates :category_id, :status_id, :shipping_id, :delivery_id, :date_id, numericality: { other_than: 1 }
+  validates :category_id, :status_id, :shipping_id, :delivery_id, :s_date_id, numericality: { other_than: 1 }
 end
