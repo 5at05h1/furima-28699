48a989234ed3508a0de7ec3c86166efa19970514
5at05h1 <dfcbvghl_3104@icloud.com>
2020-09-28 11:51:50 +0900

    テストコード記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :item do
-    
+    name            {"Apple"}
+    comment         {"ロゴ"}
+    category_id     {"家電・スマホ・カメラ"}
+    status_id       {"新品、未使用"}
+    shipping_id     {"送料込み(出品者負担)"}
+    delivery_id     {"大阪府"}
+    s_date_id       {"1~2日で発送"}
+    price           {"1000"}
   end
 end
