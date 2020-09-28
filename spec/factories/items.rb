e4bcf724f4f4b6728b88452223e23bf5545f42fb
5at05h1 <dfcbvghl_3104@icloud.com>
2020-09-28 20:02:12 +0900

    テストコード修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,12 +3,12 @@ FactoryBot.define do
     association :user
     name            {"Apple"}
     comment         {"ロゴ"}
-    category_id     {"2"}
-    status_id       {"2"}
-    shipping_id     {"2"}
-    delivery_id     {"10"}
-    s_date_id       {"2"}
-    price           {"1000"}
+    category_id     {2}
+    status_id       {2}
+    shipping_id     {2}
+    delivery_id     {10}
+    s_date_id       {2}
+    price           {1000}
     image           { Rack::Test::UploadedFile.new(File.join(Rails.root, 'app/assets/images/camera.png')) }
   end
 end
