2f5efca400583b9090320bcf7477038bc647ffdd
5at05h1 <dfcbvghl_3104@icloud.com>
2020-09-29 17:21:08 +0900

    アクション定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,11 +2,15 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
 
   def index
-    # @items = Item.all
+    @items = Item.all
   end
 
   def new
     @item = Item.new
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 
   def create
