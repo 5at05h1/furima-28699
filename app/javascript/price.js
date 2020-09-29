99251e8eb980294b63d1663cd579ed15a51518e9
5at05h1 <dfcbvghl_3104@icloud.com>
2020-09-29 12:40:19 +0900

    JS非同期処理

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,3 +1,14 @@
-window.addEventListener('input', function(){
-  "price-input"
+window.addEventListener('load', function(){
+  const price = document.getElementById("item-price")
+  const fee = document.getElementById("add-tax-price")
+  const profit =document.getElementById("profit")
+  price.addEventListener('input', function(){
+    const integer = price.value
+    console.log(Math.floor(calculation));
+    const calculation = integer * 0.1
+    console.log(Math.floor(rieki));
+    const rieki = integer - calculation
+    fee.innerHTML = calculation
+    profit.innerHTML = rieki
+  })
 })
