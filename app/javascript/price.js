7cfce1611560ceff284b3a5484f1791e79198b8f
5at05h1 <dfcbvghl_3104@icloud.com>
2020-09-29 13:04:00 +0900

    JS修正

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -5,8 +5,8 @@ window.addEventListener('load', function(){
   price.addEventListener('input', function(){
     const integer = price.value
     Math.floor(calculation)
-    const calculation = integer * 0.1
     Math.floor(rieki)
+    const calculation = integer * 0.1
     const rieki = integer - calculation
     fee.innerHTML = calculation
     profit.innerHTML = rieki
