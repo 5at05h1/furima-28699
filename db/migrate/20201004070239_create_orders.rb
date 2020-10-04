4599953f9e92b126c80e28c24abac9ce458535a7
5at05h1 <dfcbvghl_3104@icloud.com>
2020-10-04 16:11:35 +0900

    ordersマイグレーション実行

--- db/migrate/20201004070239_create_orders.rb
+++ db/migrate/20201004070239_create_orders.rb
@@ -5,9 +5,9 @@ class CreateOrders < ActiveRecord::Migration[6.0]
       t.integer    :prefectures,    null: false
       t.string     :municipality,   null: false
       t.string     :address,        null: false
-      t.string     :building,
+      t.string     :building
       t.string     :phone,          null: false
-      t.references :purchase        null: false, foreign_key: true
+      t.references :purchase,       null: false, foreign_key: true
 
       t.timestamps
     end
