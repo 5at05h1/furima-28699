6091760429eb7b0c099888778a597d09a45eb6ed
5at05h1 <dfcbvghl_3104@icloud.com>
2020-10-04 15:35:03 +0900

    マイグレーション記述

--- db/migrate/20201003124446_create_orders.rb
+++ db/migrate/20201003124446_create_orders.rb
@@ -8,6 +8,7 @@ class CreateOrders < ActiveRecord::Migration[6.0]
       t.string     :building,
       t.string     :phone,          null: false
       t.references :purchase        null: false, foreign_key: true
+      
       t.timestamps
     end
   end
