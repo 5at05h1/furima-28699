a6228a7f73e237a87b9fc90210e11983cc58efd5
5at05h1 <dfcbvghl_3104@icloud.com>
2020-10-04 15:20:18 +0900

    ordersマイグレーション記述

--- db/migrate/20201003124446_create_orders.rb
+++ db/migrate/20201003124446_create_orders.rb
@@ -1,7 +1,12 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
-
+      t.string   :postal_code,     null: false
+      t.integer  :prefectures,     null: false
+      t.string   :municipality,    null: false
+      t.string   :address,         null: false
+      t.string   :building,
+      t.string   :phone,           null: false
       t.timestamps
     end
   end
