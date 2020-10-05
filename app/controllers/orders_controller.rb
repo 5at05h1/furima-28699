149589aab21f74f979e905a4b1883cfc926005cb
5at05h1 <dfcbvghl_3104@icloud.com>
2020-10-05 15:30:25 +0900

    ルーティング、コントローラー

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,16 @@
 class OrdersController < ApplicationController
+
+  def index
+    @orders = Order.all
+  end
+
+  def create
+    @order = Order.create(order_params)
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:postal_code, :prefectures, :municipality, :address, :phone)
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :purchase
+
+  validates :postal_code, :prefectures, :municipality, :address, :phone, presence: true
+  validates :prefectures, { other_than: 1 }
 end
