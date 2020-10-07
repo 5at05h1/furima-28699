80c751a04231b95d881057a4f9bc97fd10381c0d
5at05h1 <dfcbvghl_3104@icloud.com>
2020-10-07 11:51:24 +0900

    ビューファイル修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,7 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
 
   def index
+    @item = Item.find(params[:item_id])
     @order = Order.new
   end
 
