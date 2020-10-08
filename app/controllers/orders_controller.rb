0ea5064e039940e2ca6fcec1e2b453292583f184
5at05h1 <dfcbvghl_3104@icloud.com>
2020-10-08 17:17:57 +0900

    トップページ遷移

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
   before_action :set_item, only: [:index, :create]
-  before_action :move_to_index, except: [:index, :create]
+  before_action :move_to_index, only: [:index, :create]
 
   def index
     @order = Order.new
@@ -38,8 +38,8 @@ class OrdersController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
+    if user_signed_in? && current_user.id == @item.user_id || @item.purchase.present?
+      redirect_to root_path
     end
   end
 end
