ef391611affe4e46498a55f4c38fc320a4f8b9e5
5at05h1 <dfcbvghl_3104@icloud.com>
2020-10-08 11:22:53 +0900

    テストコード

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :order do
-    
+    postal_code       {230-0045}
+    prefectures_id    {15}
+    municipality      {"横浜市鶴見区"}
+    address           {"末広町1-1"}
+    phone             {0123456789}
   end
 end
