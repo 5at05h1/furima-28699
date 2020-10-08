82a47b5c92ffa53caba6e4ff5261c0a714c006f7
5at05h1 <dfcbvghl_3104@icloud.com>
2020-10-08 19:12:49 +0900

    js修正

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -2,11 +2,15 @@ window.addEventListener('load', function(){
   const price = document.getElementById("item-price")
   const fee = document.getElementById("add-tax-price")
   const profit =document.getElementById("profit")
-  price.addEventListener('input', function(){
-    const integer = price.value
-    const calculation = integer * 0.1
-    const rieki = integer - calculation
-    fee.innerHTML = Math.floor(calculation)
-    profit.innerHTML = Math.floor(rieki)
+  // price.addEventListener('input', function(){
+  //   const integer = price.value
+  //   const calculation = integer * 0.1
+  //   const rieki = integer - calculation
+  //   fee.innerHTML = Math.floor(calculation)
+  //   profit.innerHTML = Math.floor(rieki)
+  price.addEventListener("input", () => {
+    const inputValue = document.getElementById("item-price").value;
+    fee.innerHTML = Math.floor(inputValue * 0.1).toLocaleString();
+    profit.innerHTML = Math.floor(inputValue * 0.9).toLocaleString();
   })
 })
